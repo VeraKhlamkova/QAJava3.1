a52b7f29310707a606ec75929e39e5544d892abc
Vera Khlamkova <sovasov2015@gmail.com>
2023-10-08 19:22:13 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 15895;
-        boolean woman = true;
-
-        int ratio;
-        if (woman) {
-            ratio = 20;
-        } else {
-            ratio  = 0;
-        }
+        int ratio = 20;
         int bonus = amount / ratio;
         System.out.println("Бонус за полёт: " + bonus);
     }
